f96b906ead881f5b21dfee2d693acfe136aae431
Kushagra <kushagrapanchal4573@gmail.com>
2019-01-14 17:57:02 -0600

    Day1 assignment

---
+++ src/Day1/ByteOperator.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class ByteOperator {
+
+    public static void main(String[] args) {
+
+        int intVal = 100;
+        byte byteVal = (byte) intVal;
+        byte max = 127;
+        byte min = -128;
+        byte sum = (byte) (max + min);
+
+        System.out.println(sum);
+
+    }
+}
