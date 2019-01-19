97f2b8554c2b0c9e94f77698c72cee6d5a5fa549
Kushagra <kushagrapanchal4573@gmail.com>
2019-01-19 16:55:40 -0600

    Assignment 31 added in the folder

---
+++ src/AdvancedLab/Assignment31/EmployeeInfo.java
@@ -0,0 +1,15 @@
+package AdvancedLab.Assignment31;
+
+import java.util.ArrayList;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        for(int i = 0; i < al.size(); i++){
+            System.out.println(al.get(i));
+        }
+    }
+}
